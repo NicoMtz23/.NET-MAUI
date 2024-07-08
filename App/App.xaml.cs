bd432cb06cfc096da406ee545139c5870d2d7844
NicoMtz23 <nico200martinez@gmail.com>
2024-07-08 13:01:26 -0600

    Udemy MAUI - 3.8
    Avance de clase hasta empezando el capitulo 8 de la sección 3

---
+++ App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new ContentPage()
+            {
+                Content = new Label() { Text = "Bienvenido al curso MAUI"}
+            };
+        }
+    }
+}
